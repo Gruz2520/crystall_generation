651463c35e274b3b4c3dc6a76ff9a949e2485408
Egor Gruzintsev <gruz2520@gmail.com>
2025-03-26 12:36:25 +0300

    added script for slice in alex_full

---
+++ notebooks/calculate_slices.py
@@ -0,0 +1,6 @@
+from scr.data_utils import calculate_slices_for_dataset
+import pandas as pd
+
+data = pd.read_csv("../data/alexandria/alexandria_full.csv")
+
+calculate_slices_for_dataset(data, 'cif', "../data/alexandria/alexandria_full_slice.csv")
